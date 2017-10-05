4757e5da066ff4c040a0748c5f630fe21638140b
Mads Sülau Valstorp Jørgensen <mads@sulau.dk>
2017-10-05 14:58:30 +0200

    Fix absolute path installation.

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-find . -not -wholename "*.git*" -not -name . -not -name install.sh -print0 | while IFS= read -r -d '' i; do
-	name="$(echo "$i" | sed -e 's/^\.\///' | sed -e 's/^dot//')"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+find "$DIR" -not -wholename "*.git*" -not -samefile "$DIR" -not -name install.sh -print0 | while IFS= read -r -d '' i; do
+	name="$(echo "$i" | sed -e "s,^"$DIR"/,," | sed -e 's/^dot//')"
 
 	ln -sf "$i" ~/"$name"
 done
