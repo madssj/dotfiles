1dfdd5562752d56d189c8bc67dc86348d5aa44e5
Mads Sülau Valstorp Jørgensen <mads@sulau.dk>
2017-10-05 14:49:19 +0200

    Fix bashism.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 find . -not -wholename "*.git*" -not -name . -not -name install.sh -print0 | while IFS= read -r -d '' i; do
 	name="$(echo "$i" | sed -e 's/^\.\///' | sed -e 's/^dot//')"
 
-	ln -si "$i" ~/"$name"
+	ln -sf "$i" ~/"$name"
 done
